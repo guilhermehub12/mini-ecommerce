9e26826bc8678562d8ec269f08e1011c8898ac5b
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2024-11-16 14:55:19 -0300

    feat: melhorias no index, criação das funções de atualizar e deletar do Category

--- backend/app/Http/Controllers/API/CategoryController.php
+++ backend/app/Http/Controllers/API/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -13,7 +14,8 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        return "tudo ok";
+        $categories = Category::with('products')->get();
+        return response()->json($categories);
     }
 
     /**
@@ -30,7 +32,7 @@ class CategoryController extends Controller
         if($request->hasFile('avatar')) {
             $avatar = $request->file('avatar');
             $filename = time() . '.' . $avatar->getClientOriginalExtension();
-            $avatar->storeAs('public/categories', $filename);
+            $avatar->storeAs('public/categories/', $filename);
             $category->avatar = $filename;
         }
 
@@ -48,7 +50,7 @@ class CategoryController extends Controller
      */
     public function show(string $id)
     {
-        $category = Category::with('products')->find('id', $id);
+        $category = Category::with('products')->find($id);
 
         if(!$category) {
             return response()->json([
@@ -61,16 +63,45 @@ class CategoryController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(CategoryRequest $request, string $id)
     {
-        //
+        $validated = $request->validated();
+
+        $category = Category::findOrFail($id);
+        $category->name = $validated['name'];
+        $category->description = $validated['description'];
+
+        if($request->hasFile('avatar')) {
+            $avatar = $request->file('avatar');
+            $filename = time() . '.' . $avatar->getClientOriginalExtension();
+            $avatar->storeAs('public/categories/', $filename);
+            $category->avatar = $filename;
+        }
+
+        $category->save();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Category updated successfully!',
+            'category' => $category
+        ], 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        $category = Category::find($id);
+        if (!$category) {
+            return response()->json([
+                'error' => 'Category not found!'
+            ], 404);
+        }
+
+        $category->delete();
+        return response()->json([
+            'message' => 'Category deleted!'
+        ]);
     }
 }
